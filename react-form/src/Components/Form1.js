70dbe5dad67d3182f1394e69a58377ab3595934a
jyothi <chjyothireddy97@gmail.com>
2020-11-04 11:06:31 -0600

    react form update

--- react-form/src/Components/Form1.js
+++ react-form/src/Components/Form1.js
@@ -1,64 +1,152 @@
 import React, { Component } from 'react';
 import ReactPhoneInput from "react-phone-input-2";
+import Table from "./Table";
 const initialstate ={
    
 }
 export default class Form1 extends Component{
     constructor() {
         super();
-        this.state = { name:'aa',
+        this.state = {
+            users:{
+        name:'',
         nameErr:'',
-        email:'bb',
+        email:'',
         emailErr:'',
         age:'',
         ageErr:'',
         phone: '',
-     addressErr:'',
-     stat:'',
-     statErr:''
-     }
+        address:'',
+        addressErr:'',
+        stat:'',
+        statErr:'',
+        pc:'',
+        pcErr:'',
+        Dob:'',
+        DobErr:'',
+        AppStat:''
+            }
+
+        }
         // this.ValidName=this.ValidName.bind(this);
         
     }
     ValidName(name){
         let nameErr="";
-    if(name.length<=2)
+        if(name.length==0)
+        {
+            nameErr="please enter your name ";
+        }
+     else if(name.length<=2)
        {
         nameErr="name should not less than 2 char";
+        this.setState({nameErr:nameErr})
        }
-      this.setState({nameErr:nameErr})
-      return nameErr;
+       else {
+           this.setState({name})
+       }
+     
+     
     }
     validAge(age){
         let ageErr='';
-        console.log("age>100 && age<1",(age>100 && age<1),age>100,age<1)
-        if(age<1 || age>100)
+
+        if(isNaN(age))
         {
-         ageErr="Invalid age"
+            ageErr="age should not be empty";
         }
-        this.setState({ageErr:ageErr})
+        // console.log("age>100 && age<1",(age>100 && age<1),age>100,age<1)
+       else if(age<1 || age>100)
+        {
+         ageErr="Invalid age";
+         this.setState({ageErr:ageErr})
+        }
+        else {
+            this.setState({age})
+        }
+       
     }
     validateEmail(email){
         let emailErr='';
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        const isvalid = re.test(email)
-        if(isvalid){
-            this.setState({name:this.name.value,
-               email:this.email.value})
+        if(email=='')
+        {
+            emailErr="please enter your email adress";
+            this.setState({emailErr})
         }
-        else {
-            emailErr="invalid email";
-             this.setState({emailErr:emailErr})
+        else
+        {
+            const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            const isvalid = re.test(email)
+            if(isvalid){
+                this.setState({name:this.name.value,
+                   email:this.email.value})
+            }
+            else {
+                emailErr="invalid email";
+                 this.setState({emailErr:emailErr})
+             }
+        }
+        if(email!='') {
+            this.setState({email})
+        }
+       
+        
+      }
+      //validating State 
+      validateState(stat) {
+          let statErr='';
+          if(stat=='')
+          {
+            statErr="State should not be empty"
+            this.setState({statErr})
+          }
+          else {
+              this.setState({stat})
+          }
+         
+      }
+      //validating Postalcode
+      validatePstalCode(pc) {
+          let pcErr='';
+          if(pc=='')
+          {
+            pcErr="postal code should not be empty"
+            this.setState({pcErr})
+          }
+         else {
+             this.setState({pc})
          }
+
       }
    //phone number field 
-   handleOnChange = value => {
+   PhonehandleOnChange = value => {
     console.log(value);
     this.setState({ phone: value }, () => {
       console.log(this.state.phone);
     });
   };
-
+  //Adress validation
+   validateAdress(address) {
+    let addressErr='';
+    if(address=='')
+    {
+        console.log("emptyyyyyyyy")
+        addressErr="please enter your address"
+        this.setState({addressErr})
+    }
+   }
+   //DOB validation
+   validateDob(Dob) {
+   let DobErr='';
+   if(Dob=='') {
+    DobErr="enter should not be empty"
+    this.setState({DobErr})
+   }
+   else {
+       this.setState({Dob})
+   }
+    }
+ 
 
     submitfun=()=>{
        
@@ -66,6 +154,9 @@ export default class Form1 extends Component{
         this.validAge(parseInt(this.age.value));
        this.validateEmail(this.email.value);
        this.validateState(this.stat.value);
+       this.validatePstalCode(this.pc.value);
+       this.validateAdress(this.address.value);
+       this.validateDob(this.Dob.value);
     }
     render() {
     
@@ -99,8 +190,8 @@ export default class Form1 extends Component{
       buttonStyle={{}}
       dropdownStyle={{height:'50px'}}
       country={"us"}
-      value="1425652"
-      onChange={this.handleOnChange}
+      value=""
+      onChange={this.PhonehandleOnChange}
       />
       
        address:
@@ -109,7 +200,8 @@ export default class Form1 extends Component{
            onKeyDown={this.keyPressed}
            type="text"/>
            <br></br>
-           
+           <div style={{fontSize:12, color: "red"}}>{this.state.addressErr}</div>
+
            DOB:
                  <input 
            ref={(snameInput)=>this.Dob = snameInput}
@@ -126,11 +218,20 @@ export default class Form1 extends Component{
            <br></br>
         <div style={{fontSize:12, color: "red"}}>{this.state.statErr}</div>
 
-      
+        postal Code:
+                 <input 
+           ref={(pcInput)=>this.pc = pcInput}
+           onKeyDown={this.keyPressed}
+           type="text"/>
+           <br></br>
+        <div style={{fontSize:12, color: "red"}}>{this.state.pcErr}</div>
 
         <input type='submit' value='Submit' onClick={this.submitfun} />
         <br></br>
         <p>{this.state.email}{this.state.name}</p>
+        <Table data={this.state}></Table>
+        <br></br>
+        {console.log(this.state.users)}
             </div>
         )
     }
--- react-form/src/Components/Table.js
+++ react-form/src/Components/Table.js
@@ -1,31 +1,32 @@
 import React from 'react'
 
 export default function Table(props) {
+ 
     return (
         <div>
             <p>This is table component</p>
-            {console.log((props.data))}
-            <table >
-          <tr>
-                    <th>fname</th>
-                    <th>sname</th>
-                    <th>age</th>
-                    <th>address</th>
-                    <th>phone</th>
-                    <th>email</th>
-          </tr>
-          {props.data.map((item) => (
-            <tr>
-                    <td>{item.fname}</td>
-                    <td>{item.fname}</td>
-                    <td>{item.age}</td>
-                    <td>{item.address}</td>
-                    <td>{item.phone}</td>
-                    <td>{item.email}</td>
-            </tr>
-          ))}
-        </table>
-
+            {/* {console.log((props.data))} */}
+            {/* {console.log(JSON.stringify(props.data))} */}
+            <table>
+              <tr>
+                <th>name</th>
+                <th>age</th>
+                <th>email</th>
+                <th>DOB</th>
+                <th>state</th>
+                <th>postal code</th>
+              </tr>
+              <tr>
+              <td>{props.data.name}</td>
+              <td>{props.data.age}</td>
+              <td>{props.data.Dob}</td>
+              <td>{props.data.email}</td>
+              <td>{props.data.stat}</td>
+              <td>{props.data.pc}</td>
+              </tr>
+             
+            </table>
+          
         </div>
     )
 }
